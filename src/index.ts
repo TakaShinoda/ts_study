1780783ebca9909957bf6e1f99c78a16f8402680
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-05 16:43:39 +0900

    Fix

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,37 @@ let c = {
     banana: b
 }
 let d = c.apple * 4
+let e = 1234n
+
+let f: {
+    b: number
+    c?: string
+    [key: number]: boolean
+}
+
+type Age = number
+type Person = {
+    name: string
+    age: Age
+}
+
+let age: Age = 55
+let driver: Person = {
+    name: 'Taro',
+    age: age
+}
+
+const arr1: number[] = [1,2,3]
+
+let peson: [string, string, Number] = ['Taro', 'Suzuki', 15]
+
+let trainFares: [number, number?][] = [[1.23], [12.2, 44], [10.50]]
+
+const enum Language {
+    English,
+    Japanese,
+    Russian
+}
+
+let japan = Language.Japanese
+
